a333d8882e5e9251140882150f5ca4e4c7f2e3ee
Yuta Fujii <42410688+YutaFujii0@users.noreply.github.com>
2018-11-26 10:03:45 +0900

    Revert "Added fav table. Pls migrate"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :menus, dependent: :destroy
-  has_many :favourites, dependant: :destroy
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,5 @@ Rails.application.routes.draw do
   end
   resources :results, only: [:update]
   resources :foods, only: [:index, :show]
-  resources :favourites, only: [:index, :show, :create]
 
 end
